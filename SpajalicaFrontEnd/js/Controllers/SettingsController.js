1a99faf74e77ec83092225727346fc84c37ad3cf
Ђуро Ненадовић <djuronenadovic@gmail.com>
2017-01-15 11:39:44 +0100

    Odradjen poslednji deo oko funkcionalnosti podesavanja, profila i logout funkcije. Potreban refaktor, razdvajanje kontrolera, i redizajn.

--- SpajalicaFrontEnd/js/Controllers/SettingsController.js
+++ SpajalicaFrontEnd/js/Controllers/SettingsController.js
@@ -1,56 +1,8 @@
 /**
- * Created by djnenadovic on 5.1.2017..
+ * Created by djnenadovic on 15.1.2017..
  */
 
 angular.module("SpajalicaFrontEnd", [])
-    .service('SharedData', function SharedData(){
-
-        var userData;
-
-        return userData;
-    })
-    .controller("ProfileController", function ($scope, $http, $window) {
-
-        var data = {
-            "userName": $window.sessionStorage.device
-        };
-
-        var userInfo = [];
-
-        console.log("Ucitano: " + data.userName)
-
-        $http.post('http://localhost:8000/ShowProfile', data).then(
-            function (response) {
-                if (response.data)
-                {
-                    $scope.msg = "Post Data Submitted Successfully!";
-                    console.log(response.data);
-                }
-
-                $scope.firstName = response.data.firstName == null ? "Унесите име. " : response.data.firstName;
-                $scope.lastName = response.data.lastName == null ? "Унесите презиме. " : response.data.lastName;
-
-                userInfo.push(response.data.firstName == null ? null : "Име: " + response.data.firstName);
-                userInfo.push(response.data.lastName == null ? null : "Презиме: " + response.data.lastName);
-                userInfo.push(response.data.birthDate == null ? null : "Датум рођења: " + response.data.birthDate);
-                userInfo.push(response.data.joinedDate == null ? null : "Датум приступа: " + response.data.joinedDate);
-                userInfo.push(response.data.sex == null ? null : "Пол: " + response.data.sex);
-                userInfo.push(response.data.location == null ? null : "Место: " + response.data.location);
-                userInfo.push(response.data.relationshipStatus == null ? null : "Статус везе: " + response.data.relationshipStatus);
-
-                $scope.responseObject = userInfo;
-
-            }, function (response) {
-                console.log("Service not Exists: " +
-                    response.status + "|" +
-                    response.statusText + "|");
-            });
-
-        $scope.nickname = $window.sessionStorage.device;
-    })
-    .controller("TitleController", function ($scope, $window) {
-        $scope.nickname = $window.sessionStorage.device;
-    })
     .controller("SettingsController", function ($scope, $window, $http) {
 
         var data = {
@@ -119,11 +71,5 @@ angular.module("SpajalicaFrontEnd", [])
 
                     $scope.settingsInputStyle = {'border': '3px solid red'};
                 });
-        };
-    })
-    .controller("LogoutController", function ($scope, $window) {
-        $scope.LogOut = function () {
-            $window.sessionStorage.device = null;
-            $window.location.href = './Login.html';
         };
     });
---
+++ SpajalicaFrontEnd/js/Controllers/TitleController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by djnenadovic on 15.1.2017..
+ */
+
+angular.module("SpajalicaFrontEnd", [])
+    .controller("TitleController", function ($scope, $window) {
+    $scope.nickname = $window.sessionStorage.device;
+});;
